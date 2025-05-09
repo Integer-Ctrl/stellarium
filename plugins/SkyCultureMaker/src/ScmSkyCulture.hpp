fdea1f2b14c791c6ef9937e3cabf390549cce10a
Fabian Hofer <fabianhof02@gmail.com>
2025-05-09 10:28:06 +0200

    feat: plugin structure for creating sky culture

---
+++ plugins/SkyCultureMaker/src/ScmSkyCulture.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file ScmSkyCulture.hpp
+ * @author 
+ * @brief Object holding all data of a sky culture during the creation process with the Sky Culture Maker.
+ * @version 0.1
+ * @date 2025-05-09
+ * 
+ * The minimun inforamtion that the object should store can be found in any index.json file of an existing sky culture.
+ * For example: /{userpath}/stellarium/skycultures/maya/index.json
+ */
