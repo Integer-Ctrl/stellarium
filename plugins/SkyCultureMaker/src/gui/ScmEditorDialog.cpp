4e78f8e74861a2c1b2722e4a59efc4ba7280149d
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-05-18 21:07:58 +0200

    code style and set strings to null when empty

--- plugins/SkyCultureMaker/src/gui/ScmEditorDialog.cpp
+++ plugins/SkyCultureMaker/src/gui/ScmEditorDialog.cpp
@@ -29,17 +29,33 @@ void ScmEditorDialog::createDialogContent()
 	connect(ui->titleBar, &TitleBar::closeClicked, this, &StelDialog::close);
 
 	// LABELS TAB
-	connect(ui->enNameTE, &QTextEdit::textChanged, this, [this]() {
+	connect(ui->enNameTE, &QTextEdit::textChanged, this, [this]() 
+	{
 		constellationEnglishName = ui->enNameTE->toPlainText();
 	});
-	connect(ui->natNameTE, &QTextEdit::textChanged, this, [this]() {
+	connect(ui->natNameTE, &QTextEdit::textChanged, this, [this]() 
+	{
 		constellationNativeName = ui->natNameTE->toPlainText();
+		if (constellationNativeName->isEmpty())
+		{
+			constellationNativeName = std::nullopt;
+		}
 	});
-	connect(ui->pronounceTE, &QTextEdit::textChanged, this, [this]() {
+	connect(ui->pronounceTE, &QTextEdit::textChanged, this, [this]() 
+	{
 		constellationPronounce = ui->pronounceTE->toPlainText();
+		if (constellationPronounce->isEmpty())
+		{
+			constellationPronounce = std::nullopt;
+		}
 	});
-	connect(ui->ipaTE, &QTextEdit::textChanged, this, [this]() {
+	connect(ui->ipaTE, &QTextEdit::textChanged, this, [this]() 
+	{
 		constellationIpa = ui->ipaTE->toPlainText();
+		if (constellationIpa->isEmpty())
+		{
+			constellationIpa = std::nullopt;
+		}
 	});
 	connect(ui->saveLabelsBtn, &QPushButton::clicked, this, &ScmEditorDialog::saveLabels);
 }
@@ -48,7 +64,7 @@ void ScmEditorDialog::saveLabels()
 {
 	qDebug() << "ScmEditorDialog: Saving labels:";
 	qDebug() << "	English Name:" << constellationEnglishName;
-	qDebug() << "	Native Name:" << constellationNativeName.value_or("N/A");
-	qDebug() << "	Pronounce:" << constellationPronounce.value_or("N/A");
-	qDebug() << "	IPA:" << constellationIpa.value_or("N/A");
+	if(constellationNativeName) qDebug() << "	Native Name:" << constellationNativeName.value_or("N/A");
+	if(constellationPronounce) qDebug() << "	Pronounce:" << constellationPronounce.value_or("N/A");
+	if(constellationIpa) qDebug() << "	IPA:" << constellationIpa.value_or("N/A");
 }
