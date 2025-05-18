edfe8de239d65a82542ff4a21420e7dbd0249f41
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-05-18 17:29:29 +0200

    feat: dialog has a titleBar and is movable&closeable

--- plugins/SkyCultureMaker/src/gui/StartDialog.cpp
+++ plugins/SkyCultureMaker/src/gui/StartDialog.cpp
@@ -23,4 +23,9 @@ void StartDialog::retranslate()
 void StartDialog::createDialogContent()
 {
 	ui->setupUi(dialog);
+
+	connect(&StelApp::getInstance(), SIGNAL(languageChanged()),this, SLOT(retranslate()));
+
+	connect(ui->titleBar, SIGNAL(movedTo(QPoint)), this, SLOT(handleMovedTo(QPoint)));
+	connect(ui->titleBar, &TitleBar::closeClicked, this, &StelDialog::close);
 }
